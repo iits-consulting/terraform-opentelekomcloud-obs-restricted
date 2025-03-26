6790078bd2173c598ae6469a8c40d14a8406257d
Can Aykin <can.aykin@iits-consulting.de>
2025-03-26 15:39:21 +0100

    - add descriptions for the outputs - change bucket_name output to be dependent to the resource - add sensitive flag for the sensitive outputs (they were already sensitive from the provider but this ensures they will be marked sensitive even if provider makes a mistake)

--- output.tf
+++ output.tf
@@ -1,11 +1,16 @@
 output "bucket_name" {
-  value = var.bucket_name
+  value       = opentelekomcloud_obs_bucket.bucket.bucket
+  description = "OBS bucket name passthrough with dependency chain."
 }
 
 output "bucket_access_key" {
-  value = opentelekomcloud_identity_credential_v3.user_aksk.access
+  value       = opentelekomcloud_identity_credential_v3.user_aksk.access
+  description = "OBS bucket access key for the created user. Can only access to the specific bucket and the specific KMS key used for bucket encryption."
+  sensitive   = true
 }
 
 output "bucket_secret_key" {
-  value = opentelekomcloud_identity_credential_v3.user_aksk.secret
+  value       = opentelekomcloud_identity_credential_v3.user_aksk.secret
+  description = "OBS bucket secret key for the created user. Can only access to the specific bucket and the specific KMS key used for bucket encryption."
+  sensitive   = true
 }
