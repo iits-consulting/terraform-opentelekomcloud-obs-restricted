a85fa3c910eeb5838a75191eb28f7a9106bfe965
Kevin Yu <kevin.yu@iits-consulting.de>
2025-10-07 11:29:13 +0200

    [module][obs] removes validations, since OTC API response with appropriate messages

--- variables.tf
+++ variables.tf
@@ -62,38 +62,4 @@ variable "lifecycle_rules" {
   }))
   description = "Lifecycle rules for the bucket. Default: null"
   default     = []
-
-  validation {
-    condition = alltrue([
-      for rule in var.lifecycle_rules :
-        rule.transitions == null ? true : alltrue([
-          for transition in rule.transitions: contains(["WARM", "COLD"], transition.storage_class)
-        ])
-    ])
-    error_message = "The storage_class in transition object has to be WARM or COLD"
-  }
-  validation {
-    condition = alltrue([
-      for rule in var.lifecycle_rules :
-        rule.transitions == null ? true : length(rule.transitions) <= 2
-    ])
-    error_message = "Each lifecycle rule can have at most 2 transition entries."
-  }
-  validation {
-    condition = alltrue([
-      for rule in var.lifecycle_rules :
-        rule.noncurrent_version_transitions == null ? true : alltrue([
-          for transition in rule.noncurrent_version_transitions :
-            contains(["WARM", "COLD"], transition.storage_class)
-        ])
-    ])
-    error_message = "The storage_class in noncurrent_version_transition object has to be WARM or COLD"
-  }
-  validation {
-    condition = alltrue([
-      for rule in var.lifecycle_rules :
-        rule.noncurrent_version_transitions == null ? true : length(rule.noncurrent_version_transitions) <= 2
-    ])
-    error_message = "Each lifecycle rule can have at most 2 noncurrent_version_transition entries."
-  }
 }
